b07f1be2eca4319423e43fb2eeae817bdca386d8
Nicholas Auer <nick.nickauer@gmail.com>
2019-05-25 10:57:52 -0600

    added power/life commands for luck

--- trigger.py
+++ trigger.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 import discord, sheets, random
 
 class Trigger(commands.Cog):
-    
+
     @commands.command()
     async def trigger(self, ctx, *args):
         '''Get a trigger for a character. Can specify index starting from 1.
@@ -29,7 +29,7 @@ class Trigger(commands.Cog):
         else:
             index = 0
             t = (await sheets.used(0))
-        
+
         if str(t) != "":
             await ctx.send(t)
         else:
@@ -65,7 +65,7 @@ class Trigger(commands.Cog):
                 column = 1
             elif args[0].lower() == "power":
                 column = 2
-        
+
         if column == 0:
             column = random.randint(1, 2)
 
@@ -86,10 +86,10 @@ class Trigger(commands.Cog):
                 column = 3
             elif args[0].lower() == "power":
                 column = 4
-        
+
         if column == 0:
             column = random.randint(3, 4)
-        
+
         output = (await sheets.luck(column))
 
         if column == 3:
@@ -97,7 +97,38 @@ class Trigger(commands.Cog):
         else:
             await ctx.send("*Power Flaw*: " + output)
 
-            
+    @commands.command()
+    async def power(self, ctx, p_or_f):
+        '''Get a random power perk or flaw. Optionally specify whether you want a perk or flaw.
+        '''
+        p_or_f = p_or_f.lower()
+        if p_or_f == 'perk':
+            column = 2
+        elif p_or_f == 'flaw':
+            column = 4
+        else:
+            column = random.choice((2,4))
 
+        output = (await sheets.luck(column))
+        if column == 2:
+            await ctx.send("*Power Perk*: " + output)
+        else:
+            await ctx.send("*Power Flaw*: " + output)
 
-        
+    @commands.command()
+    async def life(self, ctx, p_or_f):
+        '''Get a random life perk or flaw. Optionally specify whether you want a perk or flaw.
+        '''
+        p_or_f = p_or_f.lower()
+        if p_or_f == 'perk':
+            column = 1
+        elif p_or_f == 'flaw':
+            column = 3
+        else:
+            column = random.choice((1,3))
+
+        output = (await sheets.luck(column))
+        if column == 1:
+            await ctx.send("*Life Perk*: " + output)
+        else:
+            await ctx.send("*Life Flaw*: " + output)
