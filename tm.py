4fbf7a79d5f14cd48986504bb416c1117fed234d
Nicholas Auer <nick.nickauer@gmail.com>
2019-08-11 15:56:58 -0600

    removed data folder

--- tm.py
+++ tm.py
@@ -1,27 +1,42 @@
 from discord.ext import commands
+from shutil import copyfile
 import random, time, discord, json
 
 async def join(ctx, *args):
+    if args:
+        id = args[0]
+    else:
+        id = str(ctx.author.id)
     char = {'owner': ctx.author.display_name, 'name': None,
             'strategy': None, 'age': None, 'gender': None, 'stats': None,
             'mech': None, 'history': None}
-    with open('./tm/chars.json', 'a+') as charsfile:
-        if len(read(charsfile)):
+    with open('./tm/chars.json', 'r+') as charsfile:
+        if len(charsfile.read()):
+            charsfile.seek(0)
             chars = json.load(charsfile)
         else:
             chars = {}
-        if str(ctx.author.id) in chars:
+        if id in chars:
             await ctx.send('You already have a pilot!')
             return
-        chars[str(ctx.author.id)] = char
+    with open('./tm/chars.json', 'w+') as charsfile:
+        chars[id] = char
         json.dump(chars, charsfile)
         await ctx.send('Pilot created! Check up on them with `%tm check`')
 
 async def check(ctx, *args):
+    if args:
+        id = args[0]
+    else:
+        id = str(ctx.author.id)
+
     with open('./tm/chars.json', 'r+') as charsfile:
-        await ctx.send(json.load(charsfile)[str(ctx.author.id)])
+        await ctx.send(json.load(charsfile)[id])
 
 class TinyMech(commands.Cog):
+    def __init__ (self):
+        copyfile('./tm/chars.json','./tm/chars.json.old')
+
     @commands.command()
     async def tm(self, ctx, cmd, *args):
         if cmd == 'join':
