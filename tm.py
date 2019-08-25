0f41010f698437369bc03b2a0c153ddec96c5f0a
Nicholas Auer <nick.nickauer@gmail.com>
2019-08-24 18:52:03 -0600

    another gendery options

--- tm.py
+++ tm.py
@@ -239,7 +239,10 @@ class Pilot(Fight_Thing):
             self.id = id
             self.owner = owner
             if random.random() < 0.02:
-                self.gender = 'Non-Binary'
+                if random.random() < 0.5:
+                    self.gender = 'Non-Binary'
+                else:
+                    self.gender = 'Agender'
                 self.name = names.get_full_name()
             elif random.random() < 0.5:
                 self.gender = 'Male'
