caf251e656ce3d4354abcfe1f4a9a2558da65122
Camtist <camdrummer13@gmail.com>
2019-09-15 23:24:39 -0400

    last last update, for now

--- autologs.py
+++ autologs.py
@@ -58,6 +58,11 @@ class AutoLogs(commands.Cog):
             with open('logchansets.json', 'w+') as filechan:
                 json.dump(data, filechan)
             await ctx.send("Session number set to " + args[1] + ".")
+        if args[0] == 'reset':
+            data = {}
+            with open('logchansets.json', 'w+') as filechan:
+                json.dump(data, filechan)
+            await ctx.send("Channel settings reset.")
 
 
     @commands.command()
@@ -82,46 +87,48 @@ class AutoLogs(commands.Cog):
         thing = {}
         plyrs = []
         mark1 = False
-        mark2 = False
+        mark3 = False
         mark5 = False
         mark6 = False
         authCheck = ''
         date = datetime.datetime(int(args[0]),int(args[1]),int(args[2]))
 
         async with ctx.typing():
-            async for message in ctx.history(limit=10000,oldest_first=True,after=date):
-                if len(args) == 3:
-                    if endMarker in message.content and mark5 is True:
-                        mark6 = True
-                        mark2 = True
-                    if startMarker in message.content and mark5 is False and mark2 is False:
-                        mark5 = True
-                    elif mark5 == True and mark2 == False and '%' not in message.content and message.author.bot == False:
-                        if message.author.display_name not in plyrs and message.author.display_name != GM:
-                            plyrs.append(message.author.display_name)
-                else:
-                    await ctx.send("Error with input. Correct command format: %log [year] [month] [day]")
+            if len(args) == 3:
+                async for message in ctx.history(limit=10000,oldest_first=True,after=date):
+                        if endMarker in message.content and mark5 is True:
+                            mark6 = True
+                            break
+                        if startMarker in message.content and mark5 is False:
+                            mark5 = True
+                        if mark5 == True and '%' not in message.content and message.author.bot == False:
+                            if message.author.display_name not in plyrs and message.author.display_name != GM:
+                                plyrs.append(message.author.display_name)
+            else:
+                await ctx.send("Error with input. Correct command format: %log [year] [month] [day]")
 
         if mark5 == False:
             await ctx.send("Error: Start Marker not found.")
-            return
-        elif mark6 == False:
-            await ctx.send("Error: End Marker not found.")
-            return
+            mark3 = True
 
-        async with ctx.typing():
-            async for message in ctx.history(limit=10000,oldest_first=True,after=date):
-                if endMarker in message.content and mark1 is True:
-                    mark1 = False
-                    await ctx.send('Logs: https://docs.google.com/document/d/' + str(out[0]))
-                    return
-                if startMarker in message.content and mark1 is False:
-                    mark1 = True
-                    out = await docs.new_log_doc(thing, ctx.channel.name.capitalize(), num, plyrs)
-                elif mark1 == True and '%' not in message.content and message.author.bot == False:
-                    await asyncio.sleep(1)
-                    if message.author.display_name != authCheck:
-                        authCheck = message.author.display_name
-                        out = await docs.add_post(out, message.author.display_name, message.content.replace('||',''))
-                    else:
-                        out = await docs.append(out, message.content.replace('||',''))
+        if mark6 == False:
+            await ctx.send("Error: End Marker not found.")
+            mark3 = True
+
+        if mark3 == False:
+            async with ctx.typing():
+                async for message in ctx.history(limit=10000,oldest_first=True,after=date):
+                    if endMarker in message.content and mark1 is True:
+                        mark1 = False
+                        await ctx.send('Logs: https://docs.google.com/document/d/' + str(out[0]))
+                        return
+                    if startMarker in message.content and mark1 is False:
+                        mark1 = True
+                        out = await docs.new_log_doc(thing, ctx.channel.name.capitalize(), num, plyrs)
+                    elif mark1 == True and '%' not in message.content and message.author.bot == False:
+                        await asyncio.sleep(1)
+                        if message.author.display_name != authCheck:
+                            authCheck = message.author.display_name
+                            out = await docs.add_post(out, message.author.display_name, message.content.replace('||',''))
+                        else:
+                            out = await docs.append(out, message.content.replace('||',''))
