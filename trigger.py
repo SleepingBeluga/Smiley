e5b2cd993ebe097b2b2ae576a7c530df8e7438a8
Nicholas Auer <nick.nickauer@gmail.com>
2019-09-21 09:38:13 -0600

    claim help3

--- trigger.py
+++ trigger.py
@@ -53,7 +53,7 @@ class Triggers_And_More(commands.Cog):
 
     @commands.command()
     async def claim(self, ctx, *args):
-        '''Maves a power from triggers to used
+        '''Moves a power from triggers to used
         Format for claim: `%claim <number>, <game>, <player>, <short description of power>`
         The commas are important!'''
         astr = ' '.join(args)
