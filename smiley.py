b99966df9a87206014ec7f4834f5919e7a3d85ac
Nicholas Auer <nick.nickauer@gmail.com>
2019-11-22 10:49:02 -0700

    testing, updated message

--- smiley.py
+++ smiley.py
@@ -42,7 +42,7 @@ async def updatebot(ctx, *args):
     if 'Mod Team' in (str(role) for role in ctx.author.roles):
         repo = git.Repo('.')
         repo.remotes.origin.pull()
-        await ctx.send('Restarting!')
+        await ctx.send('Pulled repo, restarting!')
         await ctx.bot.logout()
         sys.exit(1)
 
