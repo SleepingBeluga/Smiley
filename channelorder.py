2072755911c07b1e890e555d82d7bb80388c6bc4
Nick <nick.nickauer@gmail.com>
2020-11-14 11:32:34 -0700

    relentlessly sort channels

--- channelorder.py
+++ channelorder.py
@@ -29,18 +29,23 @@ class ChanOrder(commands.Cog):
             archives = []
 
             for category in 'WeaverDice Games', 'WeaverDice Games 2', 'PactDice Games', 'PactDice Games 2', 'Archives', 'Archives 2':
-                await sortCategory(self.b.guilds[0], category)
+                await sortCategory(self.b.guilds[0], category, ctx)
 
-        await ctx.send("Channels sorted!")
+        await ctx.send("Finished sorting channels!")
 
 
-async def sortCategory(server, category):
+async def sortCategory(server, category, ctx):
     channels = [channel for channel in server.channels if channel.category and channel.category.name == category]
     if channels and len(channels):
+        start = datetime.datetime.now()
         channels.sort(key=lambda c: c.name)
         base = min(channels, key=lambda c: c.position).position
         for i, channel in enumerate(channels):
             pos = i + base
-            if pos != channel.position:
+            attempts = 0
+            while pos != channel.position and attempts < 5:
+                attempts += 1
                 await channel.edit(position=pos)
-                await asyncio.sleep(5)
+                await asyncio.sleep(10 + 5 * attempts)
+        time_spent = (datetime.datetime.now() - start).total_seconds() / 60
+        await ctx.send(f"Sorted {len(channels)} channels in {category} in {time_spent:.1f} minutes.")
