3f45af188e91ab90f47eb0d6a214fa8f3b7ea554
Nick <nick.nickauer@gmail.com>
2021-01-27 18:47:17 -0700

    fix loops dying on errors, log those errors to botlog

--- spectate.py
+++ spectate.py
@@ -1,61 +1,67 @@
-import discord, asyncio, datetime
+import discord, asyncio, datetime, traceback
 
 async def spectate_topics(b):
     await b.wait_until_ready()
     while(True):
     # Forever
+        try:
+            #Set vars
+            wdGames = []
+            pdGames = []
+            wdSpec = []
+            pdSpec = []
+            wdActive = []
+            pdActive = []
 
-        #Set vars
-        wdGames = []
-        pdGames = []
-        wdSpec = []
-        pdSpec = []
-        wdActive = []
-        pdActive = []
+            #Collect channels
+            for server in b.guilds:
+                for channel in server.channels:
+                    if channel.category != None:
+                        if channel.category.name in ('WeaverDice Games', 'WeaverDice Games 2'):
+                            wdGames.append(channel)
+                        elif channel.category.name in ('PactDice Games', 'PactDice Games 2'):
+                            pdGames.append(channel)
+                        elif channel.name == 'wd-spectating':
+                            wdSpec.append(channel)
+                        elif channel.name == 'pd-spectating':
+                            pdSpec.append(channel)
 
-        #Collect channels
-        for server in b.guilds:
-            for channel in server.channels:
-                if channel.category != None:
-                    if channel.category.name in ('WeaverDice Games', 'WeaverDice Games 2'):
-                        wdGames.append(channel)
-                    elif channel.category.name in ('PactDice Games', 'PactDice Games 2'):
-                        pdGames.append(channel)
-                    elif channel.name == 'wd-spectating':
-                        wdSpec.append(channel)
-                    elif channel.name == 'pd-spectating':
-                        pdSpec.append(channel)
+            for chan in wdGames:
+                async for message in chan.history(limit=1):
+                    if (datetime.datetime.utcnow() - message.created_at).total_seconds() < 1800:
+                        wdActive.append(chan)
 
-        for chan in wdGames:
-            async for message in chan.history(limit=1):
-                if (datetime.datetime.utcnow() - message.created_at).total_seconds() < 1800:
-                    wdActive.append(chan)
+            for chan in pdGames:
+                async for message in chan.history(limit=1):
+                    if (datetime.datetime.utcnow() - message.created_at).total_seconds() < 1800:
+                        pdActive.append(chan)
 
-        for chan in pdGames:
-            async for message in chan.history(limit=1):
-                if (datetime.datetime.utcnow() - message.created_at).total_seconds() < 1800:
-                    pdActive.append(chan)
+            if len(wdSpec) == 1:
+                top = "For active session commentary."
+                if len(wdActive) != 0:
+                    top += " Active channels: #"
+                    for chan in wdActive:
+                        top += chan.name
+                        top += ", #"
+                    top = top[:-3]
 
-        if len(wdSpec) == 1:
-            top = "For active session commentary."
-            if len(wdActive) != 0:
-                top += " Active channels: #"
-                for chan in wdActive:
-                    top += chan.name
-                    top += ", #"
-                top = top[:-3]
+                await wdSpec[0].edit(topic=top)
 
-            await wdSpec[0].edit(topic=top)
+            if len(pdSpec) == 1:
+                top = "For active session commentary."
+                if len(pdActive) != 0:
+                    top += " Active channels: #"
+                    for chan in pdActive:
+                        top += chan.name
+                        top += ", #"
+                    top = top[:-3]
 
-        if len(pdSpec) == 1:
-            top = "For active session commentary."
-            if len(pdActive) != 0:
-                top += " Active channels: #"
-                for chan in pdActive:
-                    top += chan.name
-                    top += ", #"
-                top = top[:-3]
-
-            await pdSpec[0].edit(topic=top)
-
+                await pdSpec[0].edit(topic=top)
+        except Exception as e:
+            server = b.guilds[0]
+            log_chan_cat = [cat for cat in server.categories if cat.name.lower() == 'moderation'][0]
+            log_chan = [chan for chan in log_chan_cat.channels if chan.name.lower() == 'botlog'][0]
+            tb = ''.join(traceback.format_tb(e.__traceback__))
+            error = discord.utils.escape_markdown(f'{repr(e)}')
+            await log_chan.send(f'{error} in trimhistory loop:\n```{tb}```')
         await asyncio.sleep(900)
--- trimhistory.py
+++ trimhistory.py
@@ -1,27 +1,35 @@
-import discord, asyncio, datetime
+import discord, asyncio, datetime, traceback
 
 async def channel_cleanup(b):
     await b.wait_until_ready()
     while(True):
     # Forever
-        for server in b.guilds:
-            channel_list=dict( (channel.name,channel) for channel in server.channels)
-        # Build a list of the server's channels
-        for to_clear in ('relationships', 'lgbt-plus'):
-        # For each channel we want to trim
-            mlist=[]
-            async for message in channel_list[to_clear].history(limit=None):
-                delta = (datetime.datetime.utcnow()-message.created_at)
-                tdiff= delta.days*86400 +delta.seconds
-                if tdiff >= 172800 and tdiff < 1209600:
-                    mlist.append(message)
-                    with open('output.txt','a+') as out:
-                        out.write(f'#{to_clear} {str(message.created_at)} {message.author}: {message.clean_content}\n')
-                elif tdiff >= 1209600:
-                    with open('output.txt','a+') as out:
-                        out.write(f'#{to_clear} (old) {str(message.created_at)} {message.author}: {message.clean_content}\n')
-                    await message.delete()
-            for batch_i in range((len(mlist)//100) + 1):
-                batch = mlist[batch_i:batch_i+99]
-                await channel_list[to_clear].delete_messages(batch)
+        try:
+            for server in b.guilds:
+                channel_list=dict( (channel.name,channel) for channel in server.channels)
+            # Build a list of the server's channels
+            for to_clear in ('relationships', 'lgbt-plus'):
+            # For each channel we want to trim
+                mlist=[]
+                async for message in channel_list[to_clear].history(limit=None):
+                    delta = (datetime.datetime.utcnow()-message.created_at)
+                    tdiff= delta.days*86400 +delta.seconds
+                    if tdiff >= 172800 and tdiff < 1209600:
+                        mlist.append(message)
+                        with open('output.txt','a+') as out:
+                            out.write(f'#{to_clear} {str(message.created_at)} {message.author}: {message.clean_content}\n')
+                    elif tdiff >= 1209600:
+                        with open('output.txt','a+') as out:
+                            out.write(f'#{to_clear} (old) {str(message.created_at)} {message.author}: {message.clean_content}\n')
+                        await message.delete()
+                for batch_i in range((len(mlist)//100) + 1):
+                    batch = mlist[batch_i:batch_i+99]
+                    await channel_list[to_clear].delete_messages(batch)
+        except Exception as e:
+            server = b.guilds[0]
+            log_chan_cat = [cat for cat in server.categories if cat.name.lower() == 'moderation'][0]
+            log_chan = [chan for chan in log_chan_cat.channels if chan.name.lower() == 'botlog'][0]
+            tb = ''.join(traceback.format_tb(e.__traceback__))
+            error = discord.utils.escape_markdown(f'{repr(e)}')
+            await log_chan.send(f'{error} in trimhistory loop:\n```{tb}```')
         await asyncio.sleep(3600)
